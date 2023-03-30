2521d371863584ba3e2b4e8e10b1d6a83857f0b4
damian <damian.melia@mail.utoronto.ca>
2023-03-30 08:46:07 -0400

    made some stuff happen

--- eats_utsc/eats_utsc/settings.py
+++ eats_utsc/eats_utsc/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'EmployeeApp.apps.EmployeeappConfig',
+    'food',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
